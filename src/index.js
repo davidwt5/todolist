3c02da7c11544ce4e8c9cc227f0116593391e227
davidwt5 <davidwtan75@gmail.com>
2021-08-18 12:56:54 +1000

    modularised css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import './stylesheets/reset.css'
+import './stylesheets/global.css'
+import './stylesheets/header.css'
+import './stylesheets/sidebar.css'
+import './stylesheets/main.css'
+
 import Project from './modules/Project'
 import displayController from './modules/displayController'
-import './stylesheets/reset.css'
-import './stylesheets/style.css'
