ffb8fb9a2e472cd012ae0b24a67c3901867fd5d6
davidwt5 <davidwtan75@gmail.com>
2021-08-20 23:35:48 +1000

    refactored to use mvc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import './stylesheets/reset.css'
-import './stylesheets/global.css'
-import './stylesheets/header.css'
-import './stylesheets/sidebar.css'
-import './stylesheets/main.css'
-import './stylesheets/popup.css'
+import './views/stylesheets/reset.css'
+import './views/stylesheets/global.css'
+import './views/stylesheets/header.css'
+import './views/stylesheets/sidebar.css'
+import './views/stylesheets/main.css'
+import './views/stylesheets/popup.css'
 
-import main from './modules/main';
+import displayController from './controllers/displayController';
+
+(() => {
+    document.querySelector('.projects-header .plus')
+    .addEventListener('click', () => {
+        displayController.newProjectPopupHandler();
+    });
+
+    document.querySelector('.popup-film .cancel')
+    .addEventListener('click', () => {
+        displayController.cancelNewProjectPopupHandler();
+    });
+
+    document.querySelector('.popup-film .confirm')
+    .addEventListener('click', () => {
+        displayController.addNewProjectPopupHandler();
+    });
+})();
