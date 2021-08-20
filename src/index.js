2ac888188997b912d20b2359dee6ffad7cf8de8f
davidwt5 <davidwtan75@gmail.com>
2021-08-20 19:42:01 +1000

    implemented new project popup

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './stylesheets/global.css'
 import './stylesheets/header.css'
 import './stylesheets/sidebar.css'
 import './stylesheets/main.css'
+import './stylesheets/popup.css'
 
 // import main from './modules/main';
